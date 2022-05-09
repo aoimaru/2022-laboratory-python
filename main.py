67a814f5d0b6fd81f7860a2834c1dd1c147d8b06
aoimaru <51444995+aoimaru@users.noreply.github.com>
2022-05-10 00:34:36 +0900

    モデルの作成[src]モデルの作成成功:モデル名の命名に修正必要

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+
+
 from lib.doc2vecs import D2V
+from lib.handles import Handle
+from lib.trs import TR
+
+ROOT_FILE_PATH = "../0512-results/**/*.json"
+
+def do():
+    file_paths = Handle.get_file_path(ROOT_FILE_PATH)
+    for file_path in file_paths:
+        print(file_path)
+    tr_datas = TR.create_tr_data(file_paths)
+    D2V.create_model(tr_datas, name="apt-get-install")
+    # from datetime import datetime
+    # current_time = datetime.now()
+    # current_time_str = current_time.strftime('%Y.%m.%d.%H.%M.%S')
+    # print(current_time_str)
+    # print(type(current_time_str))
+    
 
 
 
 def main():
-    D2V.do()
+    do()
 
 
 if __name__ == "__main__":
